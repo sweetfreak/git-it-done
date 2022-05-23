f551ae24d8786289d3cc99ab1c3e532433dd73b2
jesse sheehan <jessetsheehan@gmail.com>
2022-05-22 23:22:54 -0400

    added language search buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -6,6 +6,8 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var languageButtonsEl = document.querySelector("#language-buttons");
+
 var formSubmitHandler = function(event) {
     event.preventDefault();
     var username = nameInputEl.value.trim();
@@ -90,4 +92,34 @@ var displayRepos = function (repos, searchTerm) {
     }
 }
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });     
+        } else {
+            alert("Error: Github User Not Found");
+        }
+    });
+  };
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    
+    if (language) {
+        getFeaturedRepos(language);
+    }
+
+    //clear old content
+    repoContainerEl.textContent = "";
+}
+
+//event listener for submit repo
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+//event listener for choose language
+languageButtonsEl.addEventListener("click", buttonClickHandler);
+
